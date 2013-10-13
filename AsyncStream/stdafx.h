34d396d94421948fec1c3c79c9b7c39105aa4002
hearts <hearts.zhang@gmail.com>
2013-10-13 20:17:24 +0800

    - use precompiled header
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- AsyncStream/stdafx.h
+++ AsyncStream/stdafx.h
@@ -1,16 +1,14 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
 #include "targetver.h"
 #include <Windows.h>
 #include <cstdint>
 #include <ppltasks.h>
+#include <cpprest/http_client.h>
+#include <cpprest/streams.h>
+#include <cpprest/rawptrstream.h>
+
 #include "constants.hpp"
-//#include <cpprest/filestream.h>
 
 namespace dbg {
   void log(const char*fmt, ...);
