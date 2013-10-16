02b463c31e2b90ef722395c260788bcd2be5b0fa
hearts <hearts.zhang@gmail.com>
2013-10-16 23:23:57 +0800

    - add dllregisterserver and dllunregisterserver - add dllgetactivationfactory
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- HttpByteStream/dllmain.cpp
+++ HttpByteStream/dllmain.cpp
@@ -1,4 +1,7 @@
 #include <wrl.h>
+#include <atlbase.h>
+#include <strsafe.h>
+#include "resource.h"
 #pragma comment(lib, "runtimeobject.lib")
 
 using namespace Microsoft::WRL;
@@ -21,3 +24,31 @@ STDAPI DllCanUnloadNow()
 {
   return Module<InProc>::GetModule().Terminate() ? S_OK : S_FALSE;
 }
+STDAPI DllGetActivationFactory(_In_ HSTRING activatibleClassId, _COM_Outptr_ IActivationFactory** factory) {
+  return Module<InProc>::GetModule().GetActivationFactory(activatibleClassId, factory);
+}
+STDAPI DllRegisterServer() {
+  wchar_t modpath[MAX_PATH];
+  GetModuleFileNameW(current_module, modpath, MAX_PATH);
+  HRESULT hr = S_OK;
+  ComPtr<IRegistrar> reg;
+  hr = CoCreateInstance(CLSID_Registrar, nullptr, CLSCTX_INPROC, IID_IRegistrar, &reg);
+  if (SUCCEEDED(hr))
+    hr = reg->AddReplacement(L"MODULE", modpath);
+  if (SUCCEEDED(hr)) hr = reg->ResourceRegister(modpath, IDR_HTTPBYTESTREAM, L"REGISTRY");
+
+  return hr;
+}
+
+STDAPI DllUnregisterServer() {
+  wchar_t modpath[MAX_PATH];
+  GetModuleFileNameW(current_module, modpath, MAX_PATH);
+  HRESULT hr = S_OK;
+  ComPtr<IRegistrar> reg;
+  hr = CoCreateInstance(CLSID_Registrar, nullptr, CLSCTX_INPROC, IID_IRegistrar, &reg);
+  if (SUCCEEDED(hr)) hr = reg->AddReplacement(L"MODULE", modpath);
+  if (SUCCEEDED(hr)) hr = reg->ResourceUnregister(modpath, IDR_HTTPBYTESTREAM, L"REGISTRY");
+
+  return S_OK;
+}
+
