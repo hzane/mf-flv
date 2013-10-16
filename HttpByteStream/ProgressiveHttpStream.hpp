eca81d5010b448ede7e0d66389cf4ab1545adb23
hearts <hearts.zhang@gmail.com>
2013-10-16 23:24:25 +0800

    - implement progressivehttpstream
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- HttpByteStream/ProgressiveHttpStream.hpp
+++ HttpByteStream/ProgressiveHttpStream.hpp
@@ -2,28 +2,31 @@
 #include <wrl.h>
 #include <mfidl.h>
 #include "AsyncCallback.hpp"
+#include "../AsyncStream/asynchttpstream/scatter_tar_file.hpp"
 using namespace Microsoft::WRL;
 struct ProgressiveHttpStream : public RuntimeClass<RuntimeClassFlags<ClassicCom>, IMFByteStream>{
-  STDMETHODIMP GetCapabilities(DWORD *pdwCapabilities) { return 0; };
-  STDMETHODIMP GetLength(QWORD *pqwLength) { return 0; };
-  STDMETHODIMP SetLength(QWORD qwLength) { return 0; };
-  STDMETHODIMP GetCurrentPosition(QWORD *pqwPosition) { return 0; };
-  STDMETHODIMP SetCurrentPosition(QWORD qwPosition) { return 0; };
-  STDMETHODIMP IsEndOfStream(BOOL *pfEndOfStream) { return 0; };
-  STDMETHODIMP Read(BYTE *pb, ULONG cb, ULONG *pcbRead) { return 0; };
-  STDMETHODIMP BeginRead(BYTE *pb, ULONG cb, IMFAsyncCallback *pCallback, IUnknown *punkState) { return 0; };
-  STDMETHODIMP EndRead(IMFAsyncResult *pResult, ULONG *pcbRead) { return 0; };
-  STDMETHODIMP Write(const BYTE *pb, ULONG cb, ULONG *pcbWritten) { return 0; };
-  STDMETHODIMP BeginWrite(const BYTE *pb, ULONG cb, IMFAsyncCallback *pCallback, IUnknown *punkState) { return 0; };
-  STDMETHODIMP EndWrite(IMFAsyncResult *pResult, ULONG *pcbWritten) { return 0; };
-  STDMETHODIMP Seek(MFBYTESTREAM_SEEK_ORIGIN SeekOrigin, LONGLONG llSeekOffset, DWORD dwSeekFlags, QWORD *pqwCurrentPosition) { return 0; };
-  STDMETHODIMP Flush(void) { return 0; };
-  STDMETHODIMP Close(void) { return 0; };
+  STDMETHODIMP GetCapabilities(DWORD *pdwCapabilities);
+  STDMETHODIMP GetLength(QWORD *pqwLength);
+  STDMETHODIMP SetLength(QWORD qwLength) ;
+  STDMETHODIMP GetCurrentPosition(QWORD *pqwPosition);
+  STDMETHODIMP SetCurrentPosition(QWORD qwPosition);
+  STDMETHODIMP IsEndOfStream(BOOL *pfEndOfStream);
+  STDMETHODIMP Read(BYTE *pb, ULONG cb, ULONG *pcbRead);
+  STDMETHODIMP BeginRead(BYTE *pb, ULONG cb, IMFAsyncCallback *pCallback, IUnknown *punkState);
+  STDMETHODIMP EndRead(IMFAsyncResult *pResult, ULONG *pcbRead);
+  STDMETHODIMP Write(const BYTE *pb, ULONG cb, ULONG *pcbWritten);
+  STDMETHODIMP BeginWrite(const BYTE *pb, ULONG cb, IMFAsyncCallback *pCallback, IUnknown *punkState);
+  STDMETHODIMP EndWrite(IMFAsyncResult *pResult, ULONG *pcbWritten);
+  STDMETHODIMP Seek(MFBYTESTREAM_SEEK_ORIGIN SeekOrigin, LONGLONG llSeekOffset, DWORD dwSeekFlags, QWORD *pqwCurrentPosition);
+  STDMETHODIMP Flush(void);
+  STDMETHODIMP Close(void);
 
 public:
-  ProgressiveHttpStream();
+  ProgressiveHttpStream() = default;
+  HRESULT RuntimeClassInitialize(wchar_t const*url, UINT64 contentlength);
 protected:
   ~ProgressiveHttpStream() = default;
-  AsyncCallback<ProgressiveHttpStream> on_what;
-};
+  using callback_stub = AsyncCallback<ProgressiveHttpStream>;
 
+  std::shared_ptr<scatter_tar_file> _impl;
+};
